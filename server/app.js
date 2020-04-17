06201a3984098f80e4a7d3856c9853b7156fe98d
carlitoswillis <carlitoswillis@berkeley.edu>
2020-04-17 16:17:18 -0700

    create users, log in, and redirect

--- server/app.js
+++ server/app.js
@@ -15,69 +15,107 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.get('/',
+  (req, res) => {
+    res.render('index');
+  });
 
+app.get('/create',
+  (req, res) => {
+    res.render('index');
+  });
 
-app.get('/', 
-(req, res) => {
-  res.render('index');
-});
-
-app.get('/create', 
-(req, res) => {
-  res.render('index');
-});
-
-app.get('/links', 
-(req, res, next) => {
-  models.Links.getAll()
-    .then(links => {
-      res.status(200).send(links);
-    })
-    .error(error => {
-      res.status(500).send(error);
-    });
-});
-
-app.post('/links', 
-(req, res, next) => {
-  var url = req.body.url;
-  if (!models.Links.isValidUrl(url)) {
-    // send back a 404 if link is not valid
-    return res.sendStatus(404);
-  }
-
-  return models.Links.get({ url })
-    .then(link => {
-      if (link) {
-        throw link;
-      }
-      return models.Links.getUrlTitle(url);
-    })
-    .then(title => {
-      return models.Links.create({
-        url: url,
-        title: title,
-        baseUrl: req.headers.origin
+app.get('/links',
+  (req, res, next) => {
+    models.Links.getAll()
+      .then(links => {
+        res.status(200).send(links);
+      })
+      .error(error => {
+        res.status(500).send(error);
       });
-    })
-    .then(results => {
-      return models.Links.get({ id: results.insertId });
-    })
-    .then(link => {
-      throw link;
-    })
-    .error(error => {
-      res.status(500).send(error);
-    })
-    .catch(link => {
-      res.status(200).send(link);
-    });
-});
+  });
+
+app.post('/links',
+  (req, res, next) => {
+    var url = req.body.url;
+    if (!models.Links.isValidUrl(url)) {
+      // send back a 404 if link is not valid
+      return res.sendStatus(404);
+    }
+
+    return models.Links.get({ url })
+      .then(link => {
+        if (link) {
+          throw link;
+        }
+        return models.Links.getUrlTitle(url);
+      })
+      .then(title => {
+        return models.Links.create({
+          url: url,
+          title: title,
+          baseUrl: req.headers.origin
+        });
+      })
+      .then(results => {
+        return models.Links.get({ id: results.insertId });
+      })
+      .then(link => {
+        throw link;
+      })
+      .error(error => {
+        res.status(500).send(error);
+      })
+      .catch(link => {
+        res.status(200).send(link);
+      });
+  });
 
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
+var router = require('express').Router();
 
+app.get('/login',
+  (req, res) => {
+    res.render('login.ejs');
+  });
+
+app.get('/signup',
+  (req, res) => {
+    res.render('signup.ejs');
+  });
+app.post('/login', (req, res, next) => {
+  return models.Users.get({ username: req.body.username })
+    .then((results) => {
+      if (results) {
+        let attempted = req.body.password;
+        let hashedPassword = results.password;
+        let salt = results.salt;
+        if (models.Users.compare(attempted, hashedPassword, salt)) {
+          res.status(200).redirect('/');
+        } else {
+          res.status(401).redirect('/login');
+        }
+      }
+    });
+});
+
+app.post('/signup', (req, res, next) => {
+
+  return models.Users.create({ username: req.body.username, password: req.body.password })
+    .then((results) => {
+      if (results) {
+        res.status(201).redirect('/');
+      }
+    })
+    .catch((err) => {
+      if (err.code === 'ER_DUP_ENTRY') {
+        res.status(406).redirect('/signup');
+      }
+    });
+});
 
 
 /************************************************************/
