fbb1d0c2b5928fda8f555c6d13b6dd1d1cc4438e
carlitoswillis <carlitoswillis@berkeley.edu>
2020-04-18 17:19:26 -0700

    refactor again later maybe

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,7 +5,7 @@ module.exports.createSession = (req, res, next) => {
 
 
 
-  Promise.resolve(req.cookies.shortlyid)
+  return Promise.resolve(req.cookies.shortlyid)
     .then(hash => {
       if (!hash) {
         throw hash;
@@ -39,86 +39,50 @@ module.exports.createSession = (req, res, next) => {
 
 
 
+  // .resolve(req.cookies)
 
+  // cookie exists here
 
-
-
-  // // .resolve(req.cookies)
-
-  // // cookie exists here
-
-  // // if session does not exist
-  // //   do something else
+  // if session does not exist
+  //   do something else
 
   // //   //if cookie exists, assigns cookie to session
   // Promise.resolve(req.cookies.shortlyid)
   //   .then ( hash => {
+  //     if (!hash) {
+  //       return models.Sessions.create()
+  //         .then((newSession) => {
 
-  //     return models.Sessions.get({ hash: req.cookies.shortlyid})
-  //       .then((session) => {
-  //         req.session = session;
-  //         next();
-  //       });
-
+  //           return models.Sessions.get({id: newSession.insertId})
+  //             .then ((session) => {
+  //               res.cookie('shortlyid', session.hash);
+  //             });
+  //         });
+  //     } else {
+  //       return models.Sessions.get({ hash: req.cookies.shortlyid});
+  //     }
   //   })
-  //   //if session doesn't exist then make new session, update cookie and assign to session
-  //   .catch ((err) => {
-
+  //   .catch(() => {
   //     return models.Sessions.create()
-  //       .then((newSession) => {
-
-  //         return models.Sessions.get({id: newSession.insertId})
-  //           .then ((session) => {
+  //       .then((result) => {
+  //         return models.Sessions.get( { id: result.insertId} )
+  //           .tap((session) => {
   //             res.cookie('shortlyid', session.hash);
-  //             req.session = session;
-  //             next();
+  //           });
+  //       })
+  //       .catch((err)=> {
+  //         // what if the body has no username or password
+  //         return models.Users.get({username: req.body.username})
+  //           .then ((user) => {
+  //             var options = {user: {username: req.body.username}, userId: user.id};
+  //             return models.Sessions.create(options);
   //           });
   //       });
-  //   });
-
-  // //   if cookie doesn't exist, check session, then assign hash to cookie, update session
-  // return models.Sessions.get({ hash: req.cookies.shortlyid})
-  //   .then((err, result) => {
-  //     // return res.cookie(req.body.username, result.hash);
-  //     // console.log('result: ', result);
-  //     res.cookie('shortlyid', result.hash);
-  //     return result;
-  //   })
-  //   .then((session) => {
-  //     req.session = session;
-  //     next();
-  //   })
-
-  //   .catch((err)=> {
-
-  //     // what if the body has no username or password
-
-  //     return models.Users.get({username: req.body.username})
-  //       .then ((user) => {
-
-  //         var options = {user: {username: req.body.username}, userId: user.id};
-
-  //         return models.Sessions.create(options);
-  //       })
-  //       .catch ( (err) => {
-  //         return models.Sessions.create();
-  //       });
-
-  //   })
-  //   .then((result) => {
-  //     return models.Sessions.get( { id: result.insertId} )
-  //       .then((session) => {
-  //         res.cookie('shortlyid', session.hash);
-  //         return session;
-  //       });
   //   })
   //   .then((session) => {
   //     req.session = session;
   //     next();
   //   });
-
-
-
 };
 
 /************************************************************/
